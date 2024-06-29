0ff4103f64a37cafb8c2f63a4803ef91899d6dd9
Dora Morolli <doruchan@gmail.com>
2024-06-29 17:00:25 +0200

    Add setup files

---
+++ hackbcn-backhand/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hackBCN",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "geopy",
+        "mapillary"
+    ],
+    author="Dora",
+    author_email="doruchan@gmail.com",
+    description="A short description of the hackBCN project",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/doruchan/hackBCN",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
