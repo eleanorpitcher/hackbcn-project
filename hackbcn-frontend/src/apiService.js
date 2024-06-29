9ee97c5dda01201f6f5a9be05db3946cbc0559fa
Dora Morolli <doruchan@gmail.com>
2024-06-29 19:00:31 +0200

    Add server

--- hackbcn-frontend/src/apiService.js
+++ hackbcn-frontend/src/apiService.js
@@ -1,6 +1,6 @@
 // apiService.js
 
-const API_BASE_URL =  //backendURL
+const API_BASE_URL = "http://127.0.0.1:5000" //backendURL
 
 export const fetchData = () => {
   const url = // Replace with endpoint
